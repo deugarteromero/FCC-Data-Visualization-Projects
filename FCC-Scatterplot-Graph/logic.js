3a06755dbc3b0d4b3a9ba640d06ab29f3efb80a6
Leonardo M. De Ugarte R <leonardodeu@outlook.com>
2020-05-31 20:07:18 -0700

    Add plots + xScale + yScale

--- FCC-Scatterplot-Graph/logic.js
+++ FCC-Scatterplot-Graph/logic.js
@@ -12,13 +12,39 @@ getData();
 function drawChart(){
   const w = 940;
   const h = 600;
-  //padding HERe
+  const xPadding = 60;
+  const yPadding = 90;
+
+  //Scale Setup
+  const xMaxValue = d3.max(dataset, (data) => data.Year );
+  const xMinValue = d3.min(dataset, (data) => data.Year );
+
+  const yMaxValue = d3.max(dataset, (data) => (data.Seconds / 60) );
+  console.log(Number(yMaxValue.toFixed(2)));
+
+  const xScale = d3.scaleLinear()
+                   .domain([xMinValue, xMaxValue])
+                   .range([xPadding, w - xPadding]);
+
+  const yScale = d3.scaleLinear()
+                   .domain([0, Number(yMaxValue.toFixed(2))])
+                   .range([h - yPadding, yPadding]);
 
   //Chart Setup
   const chart = d3.select("svg")
   .classed("design", true)
   .attr("width", w)
   .attr("height", h);
+
+  const plots = chart.selectAll('.plot')
+                     .data(dataset)
+                     .enter()
+                     .append("circle")
+                     .classed("plot", true)
+                     .attr("cx", (data) => xScale(data.Year) ) //C
+                     .attr("cy", (data) => yScale(data.Seconds / 60) ) //C
+                     .attr("r", 5); //C
+
 
   console.log(dataset);
 };
